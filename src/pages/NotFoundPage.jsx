e773fc87b5a7b4c650c31620db565171cfd109b2
Bermet <bermetshamshievna@gmail.com>
2022-04-06 14:28:58 +0300

    lesson-4

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+function NotFoundPage() {
+    return (
+        <div>
+            <h1>
+                Страница не доступна. Вернуться на главную страницу <a href={"/"}>Главная</a>
+            </h1>
+
+        </div>
+    )
+};
+export default NotFoundPage;
